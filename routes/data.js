c2001eb90fa5fecefb294e37ed0a636688a3360e
EC2 Default User <ec2-user@ip-172-31-34-55.ap-northeast-1.compute.internal>
2022-01-17 08:21:43 +0000

    commit11

--- routes/data.js
+++ routes/data.js
@@ -1,10 +1,18 @@
+var fs=require('fs');
 var express = require('express');
-var router = express.Router();
 require('date-utils');
 var para ={
     id:0
 }
+
+var router = express.Router();
+//var qs =require("querystring");
 var dt = new Date();
+const max_num=10;
+const filename = '../mData.txt'; // データファイル名
+var message_data; // データ
+readFromFile(filename);
+var dt ;
 
 
  var data = {
@@ -19,14 +27,17 @@ var dt = new Date();
       k1p1: "0",
       k1p2: "0",
     };
+
 /* GET users listing. */
+router.get('/view',(req, res, next)=> {
+    var w={
+        title:'data/view',
+        message_data:message_data
+    };
+    res.render('data/view', w);
+});
+
 router.get('/',(req, res, next)=> {
-    
-    //if(req.session.data !=undefined)
-    //{
-    // data =req.session.data;
-    //}
-    
     res.render('data/index', data);
 });
 
@@ -42,7 +53,7 @@ router.post('/post',(req, res, next)=> {
     {
         case "0":
             
-            let dt = new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000));
+            dt= new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000));
             data.dateStr = dt.toFormat('YYYYMMDDHH24MISS');
             data.k0=req.body['k0'];
             data.k0p1= req.body['k0p1'];
@@ -52,6 +63,12 @@ router.post('/post',(req, res, next)=> {
             data.k1=req.body['k0'];
             data.k1p1= req.body['k0p1'];
             data.k1p2= req.body['k0p2'];
+            
+            //dt = new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000));
+            //data.dateStr = dt.toFormat('YYYYMMDDHH24MISS');
+            
+            addToData(data,filename,req);
+            
             break;            
     }
  
@@ -81,5 +98,41 @@ router.get('/response',(req, res, next)=> {
     res.render('data/response', data);
 });
 
-
 module.exports = router;
+
+
+
+// テキストファイルをロード
+
+
+function readFromFile(fname) {
+   fs.readFile(fname, 'utf8', function(err, d) {
+       message_data = d.split('\n');
+       console.log(d);
+       console.log(message_data);
+       console.log(message_data.length);
+ })
+}
+
+
+// データを更新
+function addToData(data, fname, request) {
+  var obj = { 'dataStr': data.dateStr, 'oya': data.oya,'k0':data.k0,'k0p1':data.k0p1,'k0p2':data.k0p2
+                                                      ,'k1':data.k1,'k1p1':data.k1p1,'k1p2':data.k1p2 };
+  var obj_str = JSON.stringify(obj);
+  console.log('add data: ' + obj_str);
+  message_data.unshift(obj_str);
+  if (message_data.length > max_num) {
+    message_data.pop();
+  }
+  saveToFile(fname);
+}
+
+// データを保存
+function saveToFile(fname) {
+  var data_str = message_data.join('\n');
+  fs.writeFile(fname, data_str, (err) => {
+    if (err) { throw err; }
+  });
+}
+
